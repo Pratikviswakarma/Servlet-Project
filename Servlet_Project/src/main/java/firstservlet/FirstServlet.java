8f715ec5ca00674e6f26903b7ae7ee482f92374a
Pratikviswakarma <pratikvishwakarma1812@gmail.com>
2025-02-24 13:08:31 +0530

    [Pratik] Add. first Servlet program using jsp

---
+++ Servlet_Project/src/main/java/firstservlet/FirstServlet.java
@@ -0,0 +1,17 @@
+package firstservlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class FirstServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out=response.getWriter();
+        out.println("<h3>Hello World Pratik \n This is my first servlet </h3>");
+        out.close();
+    }
+}
+
---
+++ Servlet_Project/src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello World! This is my first servlet</h2>
+</body>
+</html>
